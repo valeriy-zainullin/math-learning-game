d28362f01e523a619eecb3ecf1fdbabb79993d70
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2022-11-10 06:00:34 +0300

    Publish Lebesgue integral's monotonicity and Fatu's theorem.

--- analysis-3sem.js
+++ analysis-3sem.js
@@ -22,7 +22,7 @@ analysis_3sem = [
 			"$(\\forall \\varepsilon > 0)$", "$(\\exists \\delta > 0)$",
 			"$(\\forall Q, \\Delta(Q) < \\delta)$",
 			"$(\\forall \\{ t_i \\}, t_i \\in E_i)$",
-			"$\\left| \\int\\limits_E f(x) d\\mu(x) - S(Q, f, \\{ t_i \\}) \\right|$"
+			"$\\left| \\int\\limits_E f(x) d\\mu(x) - \\sum\\limits_{i = 1}^n f(t_i) \\mu(E_i) \\right| < \\varepsilon$"
 		]
 	],
 	[
@@ -41,15 +41,14 @@ analysis_3sem = [
 			"$с_1 f_1 + c_2 f_2$", "тоже", "суммируема", "на", "$E$", "и",
 			"$\\int\\limits_E (c_1 f_1(x) + c_2 f_2(x)) d\\mu(x) = c_1 \\int\\limits_E f_1(x) d\\mu(x) + c_2 \\int\\limits_E f_2(x) d\\mu(x)$"
 		]
-	],/*
+	],
 	[
 		"Монотонность интеграла Лебега",
 		["если", "мера", "$E$", "конечна", ",", "$f_1$", "и", "$f_2$", "суммируемы", "на",
-			"на", "$E$", ",", "$c_1, c_2 \\mathbb{R}$", "тогда",
-			"$с_1 f_1 + c_2 f_2$", "тоже", "суммируема", "на", "$E$", "и",
-			"$\\int\\limits_E (c_1 f_1(x) + c_2 f_2(x)) d\\mu(x) = c_1 \\int\\limits_E f_1(x) + c_2 \\int\\limits_E f_2(x)$"
+			"$E$", ",", "$(\\forall x \\in E)$", "$f_1(x) \\leqslant f_2(x)$", "тогда",
+			"$\\int\\limits_E f_1(x) d\\mu(x) \\leqslant \\int\\limits_E f_2(x) d\\mu(x)$"
 		]
-	],*/
+	],
 	[
 		"Теорема Лебега о переходе к пределу в интеграле Лебега",
 		["если", "мера", "$E$", "конечна", ",", "$\\{f_n\\}_{n = 1}^\\infty$", "измеримы",
@@ -68,5 +67,14 @@ analysis_3sem = [
 			"$E$", ",", "тогда",
 			"$\\int\\limits_E \\lim\\limits_{m \\to \\infty} f_m(x) d\\mu(x) = \\lim\\limits_{m \\to \\infty} \\int\\limits_E f_m(x) d\\mu(x)$"
 		]
-	]
+	],
+	[
+		"Теорема Фату",
+		["если", "мера", "$E$", "конечна", ",", "$\\{f_n\\}_{n = 1}^\\infty$", "измеримы",
+			"на", "$E$", ",", "$f_m(x) \\geqslant 0$", "для", "любого", "$m \\in \\mathbb{N}$",
+			"почти", "всюду", "на", "E", ",", "$\\lim\\limits_{m \\to \\infty} f_m(x) = f(x)$",
+			"почти", "всюду", "на", "E", ",", "тогда",
+			"$\\int\\limits_E f(x) d\\mu(x) \\leqslant \\underline{\\lim}\\limits_{m \\to \\infty} \\int\\limits_E f_m(x) d\\mu(x)$"
+		]
+	]	
 ]
